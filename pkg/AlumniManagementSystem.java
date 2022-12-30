c93e52d63d07d1205eb2bcb34cbf4964cd91ac9d
atharva0302 <77654326+atharva0302@users.noreply.github.com>
2022-12-30 19:51:13 +0530

    Add files via upload

---
+++ pkg/AlumniManagementSystem.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alumnimanagementsystem.pkg;
+
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author atharva
+ */
+public class AlumniManagementSystem {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    public static void main(String[] args) throws SQLException {
+        // TODO code application logic here
+        System.out.println("hello world");
+        Login lg = null;
+        lg = new Login();
+        lg.setVisible(true);
+        
+    }
+    
+}
